b4e0b34dd0d9e4625ad21c22fb96dc4abfe835d9
olikm12@gmail.com <olikm12@gmail.com>
2022-12-19 18:08:44 +0000

    my loop2

---
+++ С#_my projects/Stage1_Project1_loop2/Stage1_Project1_loop2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1_Project1_loop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gess value.");
+            Console.WriteLine("For exit enter - stop");
+
+            const int Attempt = 5; // amount of attempt
+            int counter = 0; // counter of attempt
+            string fruit = "grape"; // what you should guess
+
+            while (counter < Attempt)
+            {
+                counter++;
+                Console.WriteLine("Attempt {0}", counter);
+                string value = Console.ReadLine();
+                    if (value == "stop")
+                    {
+                    break;
+                    }
+                    if (value != fruit)
+                    {
+                    continue;    
+                    }
+                Console.WriteLine("You guess from {0} attempt", counter);
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
